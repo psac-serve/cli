517a68bb557e043132294efa12241c70d3f93136
peyang-Celeron@bot <42040068+peyang-Celeron@users.noreply.github.com>
2021-01-21 17:18:50 +0000

    Fix code-style
    Signed-off-by: peyang-Celeron@bot <42040068+peyang-Celeron@users.noreply.github.com>

--- src/commands/base.ts
+++ src/commands/base.ts
@@ -2,14 +2,18 @@ import { __ } from "i18n";
 
 import manager from "..";
 
-export abstract class Command<T extends string | undefined> {
-    protected constructor(public name: string, private _description: string, public alias: string[] = []) {
-        [ this.name, ...this.alias ].forEach((command) => {
+export abstract class Command<T extends string | undefined> 
+{
+    protected constructor(public name: string, private _description: string, public alias: string[] = []) 
+    {
+        [ this.name, ...this.alias ].forEach((command) => 
+        {
             manager.use("Command").list[0][command] = this.execute; 
         });
     }
 
-    public get description(): string {
+    public get description(): string 
+    {
         return __(this._description);
     }
 
--- src/modules/prompt.ts
+++ src/modules/prompt.ts
@@ -9,35 +9,56 @@ import Quotes from "../lib/quotes";
 
 import Module from "./base";
 
-export default class Prompt extends Module {
-    constructor() {
+export default class Prompt extends Module 
+{
+    constructor() 
+    {
         super("Prompt", "Show beauty prompts.");
     }
 
-    init(): Promise<void> {
+    init(): Promise<void> 
+    {
         this.enabled = true;
 
         return Promise.resolve();
     }
 
-    use(): (code: number) => void {
+    use(): (code: number) => void 
+    {
         const { hostname } = manager.use("Client");
 
-        return (code: number) => {
+        return (code: number) => 
+        {
             let command = "";
 
             command = readlineSync.question(chalk`{bold {blueBright.underline ${hostname}} as {cyanBright ban-server}${code !== 0 ? chalk.bold(" stopped with " + chalk.redBright(code)) : ""}}\n {magentaBright ${figures.pointer}${code !== 0 ? chalk.redBright(figures.pointer) : chalk.blueBright(figures.pointer)}${figures.pointer}} `).trim();
 
-            while (Quotes.check(command)) { command += " " + readlineSync.question(chalk`   {blueBright ${figures.pointer}}     `).trim(); }
+            while (Quotes.check(command)) 
+            
+                command += " " + readlineSync.question(chalk`   {blueBright ${figures.pointer}}     `).trim(); 
+            
             
 
-            if (command.trim() === "" || (command.startsWith("/*") && command.endsWith("*/")) || [ "#", "//", ";" ].some(value => command.trim().startsWith(value))) { return this.use()(0); } else if (command.startsWith("/*") && !command.endsWith("*/")) { console.log(chalk`{bgRedBright.black  ERROR } ` + chalk.redBright(__("This comment block must be enclosed in */."))); }
+            if (command.trim() === "" || (command.startsWith("/*") && command.endsWith("*/")) || [ "#", "//", ";" ].some(value => command.trim().startsWith(value))) 
+            
+                return this.use()(0); 
+            
+            else if (command.startsWith("/*") && !command.endsWith("*/")) 
+            
+                console.log(chalk`{bgRedBright.black  ERROR } ` + chalk.redBright(__("This comment block must be enclosed in */."))); 
+            
             
 
-            while (!command.endsWith(";")) { command += " " + readlineSync.question(chalk`   {greenBright ${figures.pointer}}     `).trim(); }
+            while (!command.endsWith(";")) 
+            
+                command += " " + readlineSync.question(chalk`   {greenBright ${figures.pointer}}     `).trim(); 
+            
             
 
-            do { command = command.slice(0, -1); }
+            do 
+            
+                command = command.slice(0, -1); 
+            
             while (command.endsWith(";"));
 
             command = command
@@ -50,25 +71,35 @@ export default class Prompt extends Module {
 
             let stopCode;
 
-            try {
+            try 
+            {
                 stopCode = manager.use("Command").commands(command.trim());
-            } catch (error) {
+            }
+            catch (error) 
+            {
                 console.log(chalk`{bgRedBright.black  ERROR } ` + chalk.redBright(__(error.message)));
 
                 stopCode = 1;
             }
 
-            if (stopCode >= 9684) { return stopCode - 9684; }
+            if (stopCode >= 9684) 
+            
+                return stopCode - 9684; 
+            
             
 
-            if (stopCode == -1) { console.log(chalk`{bgRedBright.black  ERROR } ` + chalk.redBright(__("Command not found."))); }
+            if (stopCode == -1) 
+            
+                console.log(chalk`{bgRedBright.black  ERROR } ` + chalk.redBright(__("Command not found."))); 
+            
             
 
             return this.use()(stopCode);
         };
     }
 
-    close(): Promise<void> {
+    close(): Promise<void> 
+    {
         this.enabled = false;
 
         return Promise.resolve();
