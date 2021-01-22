1216eb49f0ecb76b18c67c1a62b081202c622608
peyang-Celeron@bot <42040068+peyang-Celeron@users.noreply.github.com>
2021-01-22 14:33:41 +0000

    Fix code-style
    Signed-off-by: peyang-Celeron@bot <42040068+peyang-Celeron@users.noreply.github.com>

--- src/errors/sub-command-not-found.ts
+++ src/errors/sub-command-not-found.ts
@@ -1,7 +1,9 @@
 import { __ } from "i18n";
 
-export default class SubCommandNotFound extends Error {
-    constructor() {
+export default class SubCommandNotFound extends Error 
+{
+    constructor() 
+    {
         super(__("Subcommand not found."));
     }
 }
