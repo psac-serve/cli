f4befdee53f0bb3f8ef1668a7630acfd5db9bfa0
peyang-Celeron@bot <42040068+peyang-Celeron@users.noreply.github.com>
2021-01-23 17:12:58 +0000

    Fix code-style
    Signed-off-by: peyang-Celeron@bot <42040068+peyang-Celeron@users.noreply.github.com>

--- src/errors/database-malformed.ts
+++ src/errors/database-malformed.ts
@@ -1,7 +1,9 @@
 import { __ } from "i18n";
 
-export default class DatabaseMalformedError extends Error {
-    constructor() {
+export default class DatabaseMalformedError extends Error 
+{
+    constructor() 
+    {
         super(__("Database file malformed."));
     }
 }
