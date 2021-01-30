2a43c3c07f457e994355694b5b80353fa9529059
Potato1682 <contact@potato1682.ml>
2021-01-31 06:59:20 +0900

    fix: Help document in a heading

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -20,7 +20,8 @@ export default class Help extends Command<string> {
     public execute(options: string): number {
         const found = manager.use("Help").getHelp(options);
 
-        console.log(CliComponents.heading(chalk`Help documentation for {greenBright ${options}}\n${found}`));
+        console.log(CliComponents.heading(chalk`Help documentation for {greenBright ${options}}`));
+        console.log(found);
 
         return 0;
     }
