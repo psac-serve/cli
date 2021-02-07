c7623130928a214b7f1856296be5e788f365169c
Potato1682 <contact@potato1682.ml>
2021-02-08 01:40:11 +0900

    refactor: Add log

--- src/modules/help.ts
+++ src/modules/help.ts
@@ -1,6 +1,12 @@
+import chalk from "chalk";
+import { sprintf } from "sprintf-js";
+import { __ } from "i18n";
+
 import CliComponents from "../utils/cli/components";
 
 import CommandNotFoundError from "../errors/command-not-found";
+
+import { default as manager, flags } from "../manager-instance";
 
 import Module from "./base";
 
@@ -21,6 +27,8 @@ export default class Help extends Module {
     }
 
     public use(): { functions: [ CliHeading, CliContent, CliKeyValueContent, CliBlankLine ], helps: { [key: string]: string[] }[], getHelp: (command: string) => string } {
+        const { logger } = manager;
+
         return {
             functions: [
                 CliComponents.heading,
@@ -28,11 +36,15 @@ export default class Help extends Module {
                 CliComponents.keyValueContent,
                 CliComponents.blankLine
             ],
-            getHelp: (command: string) => (command in this.helps[0]
-                ? this.helps[0][command]
-                : (() => {
-                    throw new CommandNotFoundError();
-                })()).join("\n"),
+            getHelp: (command: string) => {
+                logger.info(sprintf(__("Getting help for %s."), chalk.cyanBright(command)), !!flags.verbose);
+
+                return command in this.helps[0]
+                    ? this.helps[0][command].join("\n")
+                    : (() => {
+                        throw new CommandNotFoundError();
+                    })();
+            },
             helps: this.helps
         };
     }
