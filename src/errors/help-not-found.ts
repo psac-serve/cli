2771a886312619d7980a173567704bc4df439cc6
peyang-Celeron@bot <42040068+peyang-Celeron@users.noreply.github.com>
2021-02-11 15:50:48 +0000

    Fix code-style
    Signed-off-by: peyang-Celeron@bot <42040068+peyang-Celeron@users.noreply.github.com>

--- src/errors/help-not-found.ts
+++ src/errors/help-not-found.ts
@@ -1,7 +1,9 @@
 import { __ } from "i18n";
 
-export default class HelpNotFoundError extends Error {
-    constructor() {
+export default class HelpNotFoundError extends Error 
+{
+    constructor() 
+    {
         super(__("Cannot find specified help document."));
     }
 }
--- src/errors/session-not-found.ts
+++ src/errors/session-not-found.ts
@@ -1,7 +1,9 @@
 import { __ } from "i18n";
 
-export default class SessionNotFoundError extends Error {
-    constructor() {
+export default class SessionNotFoundError extends Error 
+{
+    constructor() 
+    {
         super(__("Specified session not found."));
     }
 }
