8f8945a656990a7f7352d88d0872363b8ff2d15e
Potato1682 <contact@potato1682.ml>
2021-02-23 05:54:07 +0900

    fix: import/order
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/errors/parsing/expected-operator.ts
+++ src/errors/parsing/expected-operator.ts
@@ -1,5 +1,5 @@
-import ParsingError from "./base";
 import Position from "../../lang/position";
+import ParsingError from "./base";
 
 export default class ExpectedOperatorError extends ParsingError {
     public constructor(public startPosition?: Position, public endPosition?: Position) {
