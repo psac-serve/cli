251712be4b43d3814bc0aebb8413e1796ef308d0
Potato1682 <contact@potato1682.ml>
2021-03-06 11:17:00 +0900

    fix: ExpectedError message formatting

--- src/errors/lang/parsing/expected.ts
+++ src/errors/lang/parsing/expected.ts
@@ -4,6 +4,6 @@ import ParsingError from "./base";
 
 export default class ExpectedError extends ParsingError {
     public constructor(public expected: string[], public startPosition: Position, public endPosition: Position) {
-        super(`Expected ${expected.map((token, index, { length }) => (length !== 1 && index == length - 1 ? `or '${token}'` : `'${token}',`)).join(" ")}.`, startPosition, endPosition);
+        super(`Expected ${expected.map((token, index, { length }) => (length === 1 ? token : (index == length - 1 ? `or '${token}'` : `'${token}',`))).join(" ")}.`, startPosition, endPosition);
     }
 }
