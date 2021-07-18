a934ad55a37e14e5bacb3f809c04a31929d8412d
Potato1682 <contact@potato1682.ml>
2021-07-19 08:53:43 +0900

    init: use cliffy

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { parse } from "https://deno.land/std@0.101.0/flags/mod.ts";
+import { Command } from "https://deno.land/x/cliffy@v0.19.3/command/mod.ts";
 
 import { sprintf } from "https://deno.land/std@0.101.0/fmt/printf.ts";
 import * as colors from "https://deno.land/std@0.101.0/fmt/colors.ts";
@@ -6,24 +6,22 @@ import * as colors from "https://deno.land/std@0.101.0/fmt/colors.ts";
 import i18next from "https://deno.land/x/i18next/index.js";
 import resourcesToBackend from "https://deno.land/x/i18next_resources_to_backend@v1.0.0/index.js";
 import LanguageDetector from "./language-detector.ts";
+import { t } from "./translate.ts";
 
-import { isURL } from "https://deno.land/x/deno_validator@v0.0.5/mod.ts";
-
-import { getLogger, validator } from "./logger/mod.ts";
+import { validator } from "./logger/mod.ts";
 
 interface Flags {
-    _: string,
     hostname: string,
 
     version: boolean,
     help: boolean,
     verbose: boolean,
     compress: boolean,
-    ["ignore-test"]: boolean,
-    ["use-token"]: boolean
+    ignoreTest: boolean,
+    useToken: boolean
 }
 
-export let t: (key: string) => string = key => key;
+export let flags: Flags;
 
 export const main = async (...args: string[]): Promise<number> => {
     await i18next
@@ -55,60 +53,23 @@ export const main = async (...args: string[]): Promise<number> => {
             fallbackLng: "en-US"
         });
 
-    const t = i18next.t.bind(i18next);
+    const command = await new Command()
+        .name("psac")
+        .description(t("description"))
+        .arguments("<hostname:string>")
+        .option("-c, --compress", t("usage.compress"), { default: true })
+        .option("-I, --ignore-test", t("usage.ignore-test"), { default: true })
+        .option("-t, --use-token", t("usage.use-token"), { default: true })
+        .option("-v, --verbose", t("usage.verbose"), { default: true })
+        .option("-V, --version", t("usage.version"), { standalone: true })
+        .parse([ ...Deno.args, ...args ]);
 
-    const flags = parse([ ...Deno.args, ...args ], {
-        string: [
-            "_",
-
-            "hostname"
-        ],
-        boolean: [
-            "version",
-
-            "help",
-            "verbose",
-            "compress",
-            "ignore-test",
-            "use-token"
-        ],
-        alias: {
-            "version": "V",
-
-            hostname: "_",
-            help: "h",
-            verbose: "v",
-            compress: "c",
-            ["ignore-test"]: "i",
-            ["use-token"]: "t"
-        },
-        default: {
-            compress: true,
-            ["use-token"]: true
-        }
-    }) as unknown as Flags as Pick<Flags, keyof Flags>;
+    flags = { ...command.options, ...{ hostname: command.args[0] } } as unknown as Flags;
 
     if (flags.version) {
         console.log(`PSAC Client dev, ${t("description")}
 Deno ${Deno.version.deno}
 Compiled by TypeScript ${Deno.version.typescript}`);
-
-        return 0;
-    } else if (flags.help) {
-        console.log(
-            `${t("usage.usage")}: psac [${t("usage.options").toLowerCase()}] <${t("usage.hostname")}>
-
-${t("usage.options")}:
-    -c, --compress     ${t("usage.compress")}
-        --no-compress  ${t("usage.no-compress")}
-    -i, --ignore-test  ${t("usage.ignore-test")}
-    -t, --use-token    ${t("usage.use-token")}
-        --no-use-token ${t("usage.no-use-token")}
-    -h, --help         ${t("usage.help")}
-    -v, --verbose      ${t("usage.verbose")}
-    -V, --version      ${t("usage.version")}
-
-${t("usage.project")}: https://github.com/psac-serve/cli`);
 
         return 0;
     }
@@ -119,18 +80,10 @@ ${t("usage.project")}: https://github.com/psac-serve/cli`);
         return -1;
     }
 
-    if (flags._.length <= 0) {
-        if (Deno.env.get("DEBUG") !== "1") {
-            validator.error(t("startup.hostname"));
-
-            return 1;
-        }
-    }
-
     let hostname;
 
     try {
-        const hostnameFlag = flags._[0];
+        const hostnameFlag = flags.hostname;
         let protocol = "";
 
         if (!/https?:\/\//.test(hostnameFlag)) {
@@ -144,7 +97,7 @@ ${t("usage.project")}: https://github.com/psac-serve/cli`);
         return 1;
     }
 
-    if (!flags["use-token"]) {
+    if (!flags.useToken) {
         validator.warn(sprintf(t("security"), colors.bold("--use-token")));
     }
 
